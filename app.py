bc241eee0d7140e6ea2d16bb87ddcd4a317e9bed
gigi.sayfan <gigi.sayfan@helix.com>
2018-02-25 09:21:12 -0800

    The quote service now supports CORS

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 
 import hug
+from hug_middleware_cors import CORSMiddleware
 import redis
+
+api = hug.API(__name__)
+api.http.add_middleware(CORSMiddleware(api))
 
 redis_host = os.environ.get('QUOTE_STORE_SERVICE_HOST', 'localhost')
 redis_server = redis.StrictRedis(host=redis_host, port=6379, db=0)
